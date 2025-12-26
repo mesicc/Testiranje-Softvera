da0c19d6efdcda7099f7564a7527649163ee8962
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2025-12-26 09:41:02 +0100

    Kemal - z3

--- Zadaća 1/SmartLibrary/obj/Debug/net9.0/SmartLibrary.AssemblyInfo.cs
+++ Zadaća 1/SmartLibrary/obj/Debug/net9.0/SmartLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2444d0560963189eaa22f593f534c83211c7892b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23753a0ad5be32055823fa21680d14323add970a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
